749327f38203f2820658e6e514397d3d3f713070
Jagjeetsekhon3 <andiospc.com@gmail.com>
2026-06-13 15:38:12 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,12 +3,5 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
-  // Ensure route groups work correctly on Vercel
-  outputFileTracingIncludes: {
-    '/*': ['./app/**/*'],
-  },
 }
 module.exports = nextConfig
